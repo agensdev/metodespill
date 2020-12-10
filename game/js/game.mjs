267730f254a4d8e93a300553cd85971e91719ed6
Christian Simonsen <christian@agens.no>
2020-12-10 14:56:26 +0100

    Update to game engine

--- game/js/game.mjs
+++ game/js/game.mjs
@@ -10,7 +10,7 @@ export default class Game {
 
         this.state = null;
         this.scenes = null;
-
+        this.currentSceneId = null;
         this.currentSceneData = null;
         this.currentSceneUI = null;
 
@@ -23,44 +23,16 @@ export default class Game {
     }
 
     loadScene(sceneId, scenes) {
-        this.clearScene()
+        this.clearScene();
+        this.sceneId = sceneId;
         this.currentSceneUI = document.querySelector("#sceneTemplate").content.cloneNode(true);
-        this.currentSceneData = this.findScene(sceneId, this.scenes)
+        this.currentSceneData = this.findScene(sceneId, this.scenes);
+
         if (this.currentSceneData) {
-            let headerImage = this.currentSceneData.headerImage
-            if (headerImage) {
-                let img = this.currentSceneUI.querySelector("img");
-                img.src = headerImage.src;
-                img.alt = headerImage.alt;
-            }
-
-            let content = this.currentSceneData.content;
-            if (content) {
-                let contentUI = this.currentSceneUI.querySelector(".content")
-                content.forEach(element => {
-                    let node = this.createContentNode(element);
-                    if (node) {
-                        contentUI.appendChild(node);
-                    }
-                });
-            }
-
-            let actions = this.currentSceneData.actions;
-            if (actions) {
-                let actionsUI = this.currentSceneUI.querySelector(".actions");
-                actions.forEach(action => {
-                    let bt = document.createElement("button");
-                    bt.innerText = action.description
-                    bt.title = action.title
-                    bt.onclick = (e) => {
-                        console.log(action);
-                        this.loadScene(action.target, this.scenes);
-                    }
-
-                    actionsUI.appendChild(bt);
-                })
-            }
-
+            this.applyStatChanges(this.currentSceneData.statechange)
+            this.applyHeaderImage(this.currentSceneData.headerImage)
+            this.applyContent(this.currentSceneData.content);
+            this.applyActions(this.currentSceneData.actions);
         } else {
             //TODO : falback for feil. 
         }
@@ -69,9 +41,94 @@ export default class Game {
 
     }
 
+    applyHeaderImage(headerImage) {
+        if (headerImage) {
+            let img = this.currentSceneUI.querySelector("img");
+            img.src = headerImage.src;
+            img.alt = headerImage.alt;
+        }
+    }
+
+    applyContent(content) {
+        if (content) {
+            let contentUI = this.currentSceneUI.querySelector(".content")
+            content.forEach(element => {
+                if (this.validateConditions(element.conditions)) {
+                    let node = this.createContentNode(element);
+                    if (node) {
+                        contentUI.appendChild(node);
+                    }
+                }
+            });
+        }
+    }
+
+    applyActions(actions) {
+        if (actions) {
+            let actionsUI = this.currentSceneUI.querySelector(".actions");
+            actions.forEach(action => {
+
+                if (this.validateConditions(action.conditions)) {
+
+                    let bt = document.createElement("button");
+                    bt.innerText = this.parsText(action.description);
+                    bt.title = action.title;
+                    bt.onclick = (e) => {
+                        if (action.statechange) {
+                            this.applyStatChanges(action.statechange);
+                        }
+                        this.loadScene(action.target || this.sceneId, this.scenes);
+                    };
+
+                    actionsUI.appendChild(bt);
+                }
+            });
+        }
+    }
+
+    validateConditions(conditions) {
+        if (conditions == null || conditions == undefined) {
+            return true;
+        }
+        console.log(conditions)
+        return conditions.some(condition => this.state[condition.target] == condition.value);
+
+        return value;
+    }
+
+    applyStatChanges(changes) {
+        if (changes) {
+            changes.forEach(change => {
+                switch (change.type) {
+                    case "set": this.state[change.target] = change.value;
+                        break;
+
+                    case "dec": this.state[change.target] = (this.state[change.target] || 0) - change.value;
+                        break;
+
+                    case "inc": this.state[change.target] = (this.state[change.target] || 0) + change.value;
+                        break;
+
+
+                    case "apend": this.state[change.target] = (this.state[change.target] || "") + change.value;
+                        break;
+
+                    case "remove": this.state[change.target] = (this.state[change.target] || 0).replace(change.value, "");
+                        break;
+
+                    default:
+                        d("No such state opperation ${}")
+
+                }
+            })
+        }
+    }
+
     createContentNode(description) {
         let node = null;
         switch (description.type) {
+            case "dialogue": node = this.createDialogueNode(description)
+                break;
             case "text": node = this.createTextNode(description)
                 break;
             case "link": node = this.createLinkNode(description)
@@ -80,6 +137,23 @@ export default class Game {
                 break;
             default: d(`Typen ${description.type} is not a recognized type`);
         }
+        return node;
+    }
+
+    createDialogueNode(description) {
+
+        let node = document.querySelector("dialogueTemplate").cloneNode(true);
+        let portrait = node.querySelector("npcPortrait");
+        portrait.src = description.portrait;
+
+        let name = node.querySelector("#actorName");
+        name.innerText = description.npcName;
+
+
+        let dioalouge = node.querySelector(".dialogue");
+        let p = createTextNode(description);
+        dioalouge.appendChild(p);
+
         return node;
     }
 
@@ -133,7 +207,6 @@ export default class Game {
             throw ErrorMessages.no.error_no_such_game;
         }
     }
-
 
     findScene(sceneId, scenes) {
         let scene = Object.entries(scenes).find(item => item[0] == sceneId)
